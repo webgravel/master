d8bb0fcc863ee6e4cce1dd16b6bffb16fef445ea
Michał Zieliński <michal@zielinscy.org.pl>
2013-07-15 11:47:36 +0200

    interpret SSH_ORIGINAL_COMMAND

--- command.py
+++ command.py
@@ -1,7 +1,23 @@
+#!/usr/bin/env python2.7
 import sys
+import os
+import shlex
 
 def main():
-    pass
+    ssh_command = os.environ.get('SSH_ORIGINAL_COMMAND', None)
+    if ssh_command is not None:
+        client = sys.argv[1]
+        args = ['sudo', os.path.abspath(sys.argv[0])]
+        if client == '--operator':
+            pass
+        elif client.startswith('--node='):
+            args.append('--node=' + client[len('--node='):])
+        else:
+            raise ValueError('invalid client')
+        args += shlex.split(ssh_command)
+        os.execvp(args[0], args)
+    else:
+        sys.exit(repr(sys.argv))
 
 if __name__ == '__main__':
     main()
