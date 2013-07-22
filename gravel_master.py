426a1fdb12004bcdbb72091a0c846d2bc308aac2
Michał Zieliński <michal@zielinscy.org.pl>
2013-07-22 15:25:23 +0200

    fixes, get_one_node

--- gravel_master.py
+++ gravel_master.py
@@ -9,7 +9,7 @@ PATH = '/gravel/system/master'
 class Node(graveldb.Table('nodes', PATH)):
     default = dict(sshkey=None, address=None)
 
-    def setup(self):
+    def validate(self):
         if not re.match('^[a-zA-Z0-9._-]+$', self.name):
             raise ValueError('bad name')
 
@@ -40,4 +40,12 @@ def get_nodes(spec):
         node = Node(spec)
         if not node.exists:
             raise ValueError('node %s doesn\'t exist' % spec)
-        return node
+        return [node]
+
+def get_one_node(spec):
+    nodes = get_nodes(spec)
+    if not nodes:
+        raise ValueError('no nodes found')
+    elif len(nodes) > 1:
+        raise ValueError('too many nodes')
+    return nodes[0]
